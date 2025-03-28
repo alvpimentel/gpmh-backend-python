5c5c9884f73d87c44749b52e78f4dbe94c6d550a
alvpimentel <alvpimentel7@gmail.com>
2025-03-28 17:33:39 -0300

    feat: endpoint pra calcular media das notas e retornar JSON

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pandas as pd
+import os
 
 app = Flask(__name__)
+CORS(app)
 
+@app.route('/processar', methods=['POST'])
+def processar_arquivo():
+    if 'file' not in request.files:
+        return jsonify({"error": "Nenhum arquivo enviado"}), 400
+    
+    file = request.files['file']
 
-@app.route('/')
-def teste():
-    return 'teste'
+    if file.filename == '':
+        return jsonify({"error": "Nenhum arquivo selecionado"}), 400
 
+    try:
+        if file.filename.endswith('.xlsx'):
+            df = pd.read_excel(file)
+        else:
+            return jsonify({"error": "Formato de arquivo não suportado"}), 400
+        
+        # Média
+        df['media'] = df[['nota_1', 'nota_2']].mean(axis=1)
+        
+        print(jsonify(df.to_dict(orient='records')))
+
+        # Retorna JSON
+        return jsonify(df.to_dict(orient='records')) 
+    
+    except Exception as e:
+        return jsonify({"error": f"Erro ao processar o arquivo: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
